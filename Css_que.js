0dc20ea491aa86c018c8d0f69eaf2acaa6cd010c
ayushekhawat <144920054+ayushekhawat@users.noreply.github.com>
2024-06-28 14:24:29 +0530

    Add files via upload

---
+++ Css_que.js
@@ -0,0 +1,71 @@
+var data = [{
+	"question":"First artificial satellite sent in space was _______",
+	"a":"Sputnik 1",
+	"b":"Sputnik 2",
+	"c":"Explorer 1",
+	"d":"Explorer 2",
+	"ans":"a"
+},{
+	"question":"What is the name of dog sent in first flight to space ?",
+	"a":"Laika",
+	"b":"Jupiter",
+	"c":"Lucky",
+	"d":"Luna",
+	"ans":"a"
+},{
+	"question":"Space-age spinoffs includes _______",
+	"a":"smoke detectors",
+	"b":"invisible braces",
+	"c":"medical laser",
+	"d":"all of them",
+	"ans":"d"
+},{
+	"question":"A GPS is composed of how many solar powered satellites ?",
+	"a":"72",
+	"b":"27",
+	"c":"34",
+	"d":"47",
+	"ans":"b"
+},{
+	"question":"First man who set foot in space was _______",
+	"a":"Yuri Gagarian",
+	"b":"Jules Verne",
+	"c":"Neil Armstrong",
+	"d":"Aldrin",
+	"ans":"a"
+},{
+	"question":"Which Space probe discovered crates on moon ?",
+	"a":"Voyager",
+	"b":"Pioneer",
+	"c":"Clementine",
+	"d":"Venera",
+	"ans":"c"
+},{
+	"question":"In 1966 first space probe which landed on moon was _______",
+	"a":"Luna 1",
+	"b":"Luna 9",
+	"c":"Sputnik 1",
+	"d":"Terra 1",
+	"ans":"b"
+},{
+	"question":"A long-term platform which provides medium for research in space is called ",
+	"a":"spacecraft",
+	"b":"spacestation",
+	"c":"spaceshuttle",
+	"d":"spacesatellite",
+	"ans":"b"
+},{
+	"question":"Flight of first space craft lasted for _______",
+	"a":"180 mins",
+	"b":"108 mins",
+	"c":"80 mins",
+	"d":"8 mins",
+	"ans":"a"
+},{
+	"question":"Upward pushing of aircraft by engine is called its ",
+	"a":"velocity",
+	"b":"centrifugal force",
+	"c":"gravity",
+	"d":"thrust",
+	"ans":"d"
+}]
